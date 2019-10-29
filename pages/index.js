60d5f4b34f19843f188ae6a57cb0a38b3c04d327
Nicklaspoke <Nicklas.poke@gmail.com>
2019-10-29 12:49:52 +0100

    changed index-page animation

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import MainLayout from '../components/MainLayout';
 const Index = () => (
     <div>
         <MainLayout>
-        <div className='index-container slide-in-left'>
+        <div className='index-container slide-in-right'>
             <h1>Welcome to the BTH SERL booking system</h1>
             <h2>If you already have an account, use the login button</h2>
             <h2>If you are a new student and want to apply for an account, use the register button</h2>
